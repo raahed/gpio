4107db0dacbd7d30983520a53932edce5fa16e9f
raah <fettemarvin@gmail.com>
2017-12-11 15:05:56 +0100

    Define the main functions to use the gpio class

--- src/gpio/GPIO.php
+++ src/gpio/GPIO.php
@@ -6,5 +6,27 @@ namespace GPIO;
 
 final class GPIO {
 
+    const GPIO_PATH = '/sys/class/gpio';
+    
+    public function __construct() {
+        
+    
+    }
 
+    public function define($number) {
+        
+    }
+
+    public function get($number) {
+        
+    }
+    
+    public function set($number,$value) {
+        
+    }
+    
+    public function reset($number) {
+        
+    }
+    
 }
