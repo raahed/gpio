78a92d0a4a7e9da326f316b48884ef61c46f1d78
raah <fettemarvin@gmail.com>
2017-12-11 15:39:14 +0100

    Update FileHandler.php

--- src/gpio/FileHandler/FileHandler.php
+++ src/gpio/FileHandler/FileHandler.php
@@ -4,5 +4,18 @@ namespace GPIO\FileHandler;
 
 class FileHandler {
 
+    public function put($file,$content = '') {
+        
+        
+    }
 
+    public function get($file) {
+        
+        
+    }
+    
+    protected function fileStream($file,$mode,$content) {
+        
+        
+    }
 }
