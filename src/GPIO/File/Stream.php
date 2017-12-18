21c49ffccd6ee2fda7fa565ec87ba96ca9919005
raah <fettemarvin@gmail.com>
2017-12-18 13:36:09 +0100

    Create stream skeleton

--- src/GPIO/File/Stream.php
+++ src/GPIO/File/Stream.php
@@ -4,4 +4,25 @@ namespace \GPIO\File;
 
 class Stream {
 	
+	public const STREAM_PERM = 001;
+	
+	protected $stream = null;
+	
+	public function open($context,$flags) {
+	    
+	}
+	
+	public function close() {
+	    
+	}
+	
+	public function read() {
+	    
+	    
+	}
+	
+	public function write($content = '') {
+	    
+	}
+	
 }
