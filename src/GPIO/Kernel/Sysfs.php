5735fb02bdb27da783ee12b40cb00cb787aa3cd0
raah <fettemarvin@gmail.com>
2017-12-19 17:46:35 +0100

    Add the value and direction functions to sysfs

--- src/GPIO/Kernel/Sysfs.php
+++ src/GPIO/Kernel/Sysfs.php
@@ -15,7 +15,7 @@ class Sysfs extends Chip {
 		$stream = new \GPIO\File\Stream('export', \GPIO\File\Stream::FLAG_STREAM_WRITE);
 		
 		$stream->write($port, true);
-		
+	
 	}
 	
 	static public function unexport($port) {
@@ -29,14 +29,47 @@ class Sysfs extends Chip {
 		$stream = new \GPIO\File\Stream('unexport', \GPIO\File\Stream::FLAG_STREAM_WRITE);
 		
 		$stream->write($port, true);
+	
+	}
+	
+	static public function direction($port, $value) {
+
+        if(!$port) {
+            
+            return;
+            
+        }
+
+		$stream = new \GPIO\File\Stream('gpio'.$port.'/direction', \GPIO\File\Stream::FLAG_STREAM_WRITE);
+		
+		$stream->write($value, true);
 		
 	}
 	
-	static public function direction($value) {
+	static public function value($port,$value) {
+	    
+        if(!$port) {
+            
+            return;
+            
+        }
+
+        #TODO: check binary!
+
+        if($value) {
+            
+            $stream = new \GPIO\File\Stream('gpio'.$port.'/value', \GPIO\File\Stream::FLAG_STREAM_WRITE);
 		
-	}
-	
-	static public function value($port) {
+		    $stream->write($value,true);	  
+            
+            
+        } else {
+            
+            $stream = new \GPIO\File\Stream('gpio'.$port.'/value', \GPIO\File\Stream::FLAG_STREAM_READ);
+		
+		    return $stream->read(true);	  
+            
+        }
 	    
 	}
 	
