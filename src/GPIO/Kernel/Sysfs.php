fbf76b479f71792a765a66831841b92028e89488
raah <fettemarvin@gmail.com>
2017-12-19 17:31:15 +0100

    Edit export and unexport functions

--- src/GPIO/Kernel/Sysfs.php
+++ src/GPIO/Kernel/Sysfs.php
@@ -5,14 +5,30 @@ namespace GPIO\Kernel;
 class Sysfs extends Chip {
 	
 	static public function export($port) {
-		
-		$stream = new \GPIO\File\Stream('export');
+
+        if(!$port) {
+            
+            return;
+            
+        }
+        
+		$stream = new \GPIO\File\Stream('export', \GPIO\File\Stream::FLAG_STREAM_WRITE);
 		
 		$stream->write($port, true);
 		
 	}
 	
 	static public function unexport($port) {
+
+        if(!$port) {
+            
+            return;
+            
+        }
+
+		$stream = new \GPIO\File\Stream('unexport', \GPIO\File\Stream::FLAG_STREAM_WRITE);
+		
+		$stream->write($port, true);
 		
 	}
 	
